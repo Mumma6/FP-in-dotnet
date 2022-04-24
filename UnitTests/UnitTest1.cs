29017f0da45dfb9144fc8e3b695ea7c4a693d605
Martin <moarte6@gmail.com>
2022-04-24 17:05:15 +0200

    Added pipeline, compose and reduce stuff

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using ConsoleApp;
 using Xunit;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace UnitTests;
 
@@ -60,5 +62,70 @@ public class UnitTest1
     var expected = new List<int>() { 10, 20, 30 };
 
     Assert.Equal(expected, actual);
+  }
+
+  [Fact]
+  public void TestingExtensions()
+  {
+    var numbers = Enumerable.Range(1, 10);
+
+    var isOdds = numbers.FilterWithWhereExpressions(x => x % 2 != 0);
+    var odds = new List<int>() { 1, 3, 5, 7, 9 };
+
+    var dubbels = numbers.TransformWithOperation(x => x * 2);
+    var dubs = new List<int>() { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+
+    var added = numbers.TransformWithOperation(Extensions.AddTo(3).Compose(Extensions.RemoveTo(1)));
+
+    var addedActual = new List<int>() { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+    Assert.Equal(isOdds, odds);
+    Assert.Equal(dubbels, dubs);
+    Assert.Equal(added, addedActual);
+  }
+
+  [Fact]
+  public void TestingPipe()
+  {
+
+    // normal pipe
+    var num = 10;
+    var newNum = num.PipeAddTo(10).PipeAddTo(10);
+    var actual = 30;
+
+    // with lamba
+    Func<int, int> timesTwo = x => x * 2;
+    Func<int, int> add10 = x => x + 10;
+
+    int value = 5;
+    int resultA = value.PerformOperation(timesTwo).PerformOperation(add10).PerformOperation(x => x + 3);
+
+    var actualValue = 23;
+
+    Assert.Equal(newNum, actual);
+    Assert.Equal(resultA, actualValue);
+  }
+
+  [Fact]
+  public void TestingCustomReduceFunctions()
+  {
+    var robot1 = new Robot(name: "Robot1", batteryLevel: 60);
+    var robot2 = new Robot(name: "Robot2", batteryLevel: 45);
+    var robot3 = new Robot(name: "Robot3", batteryLevel: 95);
+    var robot4 = new Robot(name: "Robot4", batteryLevel: 27);
+
+    ImmutableList<Robot> robots = ImmutableList.Create(new Robot[] { robot1, robot2, robot3, robot4 });
+
+    var lowBattery = robots.Min(x => x.BatteryLevel);
+    var highBattery = robots.Max(x => x.BatteryLevel);
+    var minVal = 27;
+    var highVal = 95;
+
+    var totaltBatteryLevel = robots.Select(robot => robot.BatteryLevel).Aggregate((first, second) => first + second);
+    var batteryVal = 60 + 45 + 95 + 27;
+
+    Assert.Equal(lowBattery, minVal);
+    Assert.Equal(highBattery, highVal);
+    Assert.Equal(totaltBatteryLevel, batteryVal);
   }
 }
